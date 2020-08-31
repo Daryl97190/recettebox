33f9a86b2df7326437ec5edf1bb67e7202b5fb88
DARYL LIVIO <58260833+Daryl97190@users.noreply.github.com>
2020-08-31 12:04:34 +0200

    Update base.jsx

--- src/base.jsx
+++ src/base.jsx
@@ -3,13 +3,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseApp = firebase.initializeApp( {
-    apiKey: "AIzaSyAZBrefDMiXnUAs4fucipa8OR9kb3N3HK0",
-    authDomain: "recettebox-c54d4.firebaseapp.com",
-    databaseURL: "https://recettebox-c54d4.firebaseio.com",
-    projectId: "recettebox-c54d4",
-    storageBucket: "recettebox-c54d4.appspot.com",
-    messagingSenderId: "1030484441611",
-    appId: "1:1030484441611:web:2137dd8f7252338440250e"
+    apiKey: "xxxxxx",
+    authDomain: "xxxxxx",
+    databaseURL: "xxxxxx",
+    projectId: "xxxxxx",
+    storageBucket: "xxxxxx",
+    messagingSenderId: "xxxxxx",
+    appId: "xxxxxx"
 })
 const base = Rebase.createClass(firebaseApp.database());
 
